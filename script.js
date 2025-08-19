c1af4e157d9200e1f8b24b5b7d00e98ad8fad85d
gxldnn <riberajan@gmail.com>
2025-08-19 18:34:54 +0200

    asd

--- script.js
+++ script.js
@@ -64,12 +64,12 @@ const buildingData = {
         title: "Edificio en La Ràpita - Piso 2",
         subtitle: "Segunda planta del edificio en Sant Carles de la Ràpita, España",
         images: [
-            "images/1/main.jpg",
-            "images/1/1.jpg",
-            "images/1/2.jpg",
-            "images/1/3.jpg",
-            "images/1/4.jpg",
-            "images/1/5.jpg"
+            "images/2/main.jpg",
+            "images/2/1.jpg",
+            "images/2/2.jpg",
+            "images/2/3.jpg",
+            "images/2/4.jpg",
+            "images/2/5.jpg"
         ],
         characteristics: {
             specs: [
@@ -115,12 +115,12 @@ const buildingData = {
         title: "Edificio en La Ràpita - Piso 3",
         subtitle: "Tercera planta del edificio en Sant Carles de la Ràpita, España",
         images: [
-            "images/1/main.jpg",
-            "images/1/1.jpg",
-            "images/1/2.jpg",
-            "images/1/3.jpg",
-            "images/1/4.jpg",
-            "images/1/5.jpg"
+            "images/3/main.jpg",
+            "images/3/1.jpg",
+            "images/3/2.jpg",
+            "images/3/3.jpg",
+            "images/3/4.jpg",
+            "images/3/5.jpg"
         ],
         characteristics: {
             specs: [
